f3527e86b103ee4434b2e6b546802604e3b09161
Kithicide <165344707+Kithicide@users.noreply.github.com>
2024-03-29 09:13:47 -0400

    bot.js

--- bot.js
+++ bot.js
@@ -10,8 +10,8 @@
 
    # MADE BY RTX!! FEEL FREE TO USE ANY PART OF CODE
    ## FOR HELP CONTACT ME ON DISCORD
-   ## Contact    [ DISCORD SERVER :  https://discord.gg/FUEHs7RCqz ]
-   ## YT : https://www.youtube.com/channel/UCPbAvYWBgnYhliJa1BIrv0A
+   ## Contact    [ DISCORD SERVER :  https://discord.gg/gQjtJkJds4 ]
+   ## YT : https://www.youtube.com/channel/UCMI8MhYwwfqYC9M7AZAIDug
 */
 
 const { Client, GatewayIntentBits, Partials } = require("discord.js");
